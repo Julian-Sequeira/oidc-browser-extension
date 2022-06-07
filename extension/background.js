5fa7d71a47978eccd15e02be3f67953cd3b4ef43
Julian <Julian Sequeira>
2022-06-07 19:52:38 -0400

    Created notifications by using setBadgeText

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,37 @@
-let responseHandler = function(details) {
+async function getTabFromCurrentWindow() {
+    return new Promise((resolve) => {
+        chrome.tabs.query(
+            {active: true, currentWindow: true},
+            (tabs) => { resolve(tabs); }
+        )
+    });
+}
+
+async function notifyFlowStart() {
+    // Pop up a notification for the user
+    let [tab] = await getTabFromCurrentWindow();
+    console.log(tab);
+    chrome.browserAction.setBadgeText({
+        text: "1",
+        tabId: tab.id
+    })
+    chrome.browserAction.setBadgeBackgroundColor({
+        color: "#ff0000"
+    })
+}
+
+
+let responseHandler = async function(details) {
     console.log("Interrupted Redirect Here");
     if (details.statusCode == 302) {
+
+        // Get the RP parameters to start OIDC flow
         details.responseHeaders.forEach((header, i) => {
             if (header.name.toLowerCase() == "location") {
                 console.log(header.value);
             }
         })
+        notifyFlowStart();
     }
 }
 
